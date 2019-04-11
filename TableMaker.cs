bad1d67a74651617c17f8a9811f68225b1758f39
David Martin <dmartin@utk.edu>
2019-04-11 14:42:57 -0400

    nvarchars

--- TableMaker.cs
+++ TableMaker.cs
@@ -22,11 +22,11 @@ public class TableMaker
     {
         fields = new List<ColumnDef>();
         fields.Add(item: new ColumnDef() { name = "CreatedOn", type = "DateTime2", nully = "NULL" });
-        fields.Add(item: new ColumnDef() { name = "CreatedBy", type = "varchar", size = 20, nully = "NULL" });
+        fields.Add(item: new ColumnDef() { name = "CreatedBy", type = "nvarchar", size = 32, nully = "NULL" });
         fields.Add(item: new ColumnDef() { name = "EditedOn", type = "DateTime2", nully = "NULL" });
-        fields.Add(item: new ColumnDef() { name = "EditedBy", type = "varchar", size = 20, nully = "NULL" });
+        fields.Add(item: new ColumnDef() { name = "EditedBy", type = "nvarchar", size = 32, nully = "NULL" });
         fields.Add(item: new ColumnDef() { name = "DeletedOn", type = "DateTime2", nully = "NULL" });
-        fields.Add(item: new ColumnDef() { name = "DeletedBy", type = "varchar", size = 20, nully = "NULL" });
+        fields.Add(item: new ColumnDef() { name = "DeletedBy", type = "nvarchar", size = 32, nully = "NULL" });
     }
 
     public TableMaker(string tablename)
@@ -47,9 +47,9 @@ public class TableMaker
         }
         else if (this.tableName.StartsWith("lst"))
         {
-            ColumnDef name = new ColumnDef(){name = "Name", type = "varchar", size = 50, nully = "NOT NULL" };
+            ColumnDef name = new ColumnDef(){name = "Name", type = "nvarchar", size = 50, nully = "NOT NULL" };
             this.fields.Insert(0, name);            
-            ColumnDef code = new ColumnDef(){name = "Code", type = "varchar", size = 10, nully = "NULL" };
+            ColumnDef code = new ColumnDef(){name = "Code", type = "nvarchar", size = 16, nully = "NULL" };
             this.fields.Insert(0, code);
             ColumnDef key = new ColumnDef() { name = "ID", type = "int", nully = "IDENTITY(1,1) PRIMARY KEY" };
             this.fields.Insert(0, key);
@@ -75,27 +75,28 @@ public class TableMaker
     }
 }
 
-public class ColumnDef
-{
-    public string name { get; set; }
-    public string type { get; set; }
-    public int size { get; set; }
-    public string nully { get; set; }
-
-    public override string ToString()
+    public class ColumnDef
     {
-        string def = String.Empty;
-        switch (type.ToLower())
+        public string name { get; set; }
+        public string type { get; set; }
+        public int size { get; set; }
+        public string nully { get; set; }
+
+        public override string ToString()
         {
-            case "varchar":
-                def = $"    [{name}] {type}({size}) {nully}";
-                break;
-            default:
-                def = $"    [{name}] {type} {nully}";
-                break;
+            string def = String.Empty;
+            switch (type.ToLower())
+            {
+                case "varchar":
+                case "nvarchar":
+                    def = $"    [{name}] {type}({size}) {nully}";
+                    break;
+                default:
+                    def = $"    [{name}] {type} {nully}";
+                    break;
+            }
+            return def;
         }
-        return def;
     }
-}
 
 }
